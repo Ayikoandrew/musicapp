d60b41ffe2232ee2cade705d2b7adf2d5df237c7
andrew <andrewayiko40@gmail.com>
2024-08-28 16:07:08 +0300

    change of database name

--- server/src/server/database.py
+++ server/src/server/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:Ayiko123?!@localhost:5432/fluttermusicapp"
+DATABASE_URL = "postgresql://postgres:Ayiko123?!@localhost:5432/musicapp"
 
 engine = create_engine(
     DATABASE_URL,
