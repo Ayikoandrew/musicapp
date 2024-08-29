bb7e0532c3d8e9a1edbd4824cb4732afdce2a66c
andrew <andrewayiko40@gmail.com>
2024-08-29 11:41:17 +0300

    schema for login

--- server/src/server/routes/auth.py
+++ server/src/server/routes/auth.py
@@ -5,12 +5,13 @@ from sqlalchemy.orm import Session
 from server.database import get_db
 from server.models.user import User
 from server.pydantic_schema.create_user import CreateUser
+from server.pydantic_schema.login_user import LoginUser
 
 
 router = APIRouter(tags=["Authentication"])
 
 
-@router.post("/signup")
+@router.post("/signup", status_code=status.HTTP_201_CREATED)
 def signup(user: CreateUser, db: Session = Depends(get_db)):
     user_db = db.query(User).filter(User.email == user.email).first()
 
@@ -34,5 +35,29 @@ def signup(user: CreateUser, db: Session = Depends(get_db)):
     db.add(user_db)
     db.commit()
     db.refresh(user_db)
+
+    return user_db
+
+
+@router.post("/login", status_code=status.HTTP_202_ACCEPTED)
+def login(user: LoginUser, db: Session = Depends(get_db)):
+    user_db = db.query(User).filter(User.email == user.email).first()
+
+    if not user_db:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid credentials",
+        )
+
+    isPassword = bcrypt.checkpw(
+        password=user.password.encode(),
+        hashed_password=user_db.password,
+    )
+
+    if not isPassword:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid credentials",
+        )
 
     return user_db
